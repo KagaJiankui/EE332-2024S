0d3966fd8e8baf324df15894cbced03fba480291
KagaJiankui <FalkenSakurai@outlook.com>
2024-02-27 14:54:18 +0800

    init lab2

---
+++ lab2/lab2.srcs/sim_1/new/FA_tb.v
@@ -0,0 +1,51 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company:
+// Engineer:
+//
+// Create Date: 02/27/2024 02:53:28 AM
+// Design Name:
+// Module Name: FA_tb
+// Project Name:
+// Target Devices:
+// Tool Versions:
+// Description:
+//
+// Dependencies:
+//
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module FA_tb;
+
+  reg  [2:0] data;
+  wire [1:0] out_g;
+  wire [1:0] out_lut;
+
+  full_adder_g fag (
+      .c0(data[0]),
+      .a (data[1]),
+      .b (data[2]),
+      .s (out_g[0]),
+      .c1(out_g[1])
+  );
+
+  full_adder_lut fa_lut (
+      .in_1 (data),
+      .out_1(out_lut)
+  );
+
+  initial begin
+    data=3'b000;
+  end
+
+  always begin
+    data={$urandom}/8;
+    #5;
+  end
+
+endmodule
---
+++ lab2/lab2.srcs/sources_1/new/full_adder_g.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: SUSTech-EE332
+// Engineer: pachina
+//
+// Create Date: 02/27/2024 02:53:02 AM
+// Design Name: Full Adder using Gate-level RTL
+// Module Name: full_adder_g
+// Project Name:
+// Target Devices: XC7A100T-1CSG324C
+// Tool Versions:
+// Description:
+// Full adder using gate-level RTL simulation.
+// $$ FA:{c_0,a,b}|->{c_1,s} $$
+// $$ c_1=AB+BC+CA$$
+//
+// Dependencies:
+//
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module full_adder_g (
+    input  wire c0,
+    input  wire a,
+    input  wire b,
+    output wire s,
+    output wire c1
+);
+
+  assign s  = ((~c0) & (~a) & b) | ((~c0) & a & (~b)) | (c0 & (~a) & (~b)) | (c0 & a & b);
+  assign c1 = (c0 & a) | (a & b) | (c0 & b);
+
+endmodule
