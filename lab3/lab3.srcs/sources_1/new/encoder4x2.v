1bd6e4b38d933d02071b204a53e62dee560d853e
KagaJiankui <FalkenSakurai@outlook.com>
2024-03-18 20:25:40 +0800

    modify encoder4x2 to pass timing sim

--- lab3/lab3.srcs/sources_1/new/encoder4x2.v
+++ lab3/lab3.srcs/sources_1/new/encoder4x2.v
@@ -6,10 +6,19 @@ module encoder4x2 (
     output reg v
 );
 
-  always @(in) begin
-    v = in[3] | in[2] | in[1] | in[0];
-    out[0] = v ? (in[3] | (~in[2] & in[1])) : 1'bz;
-    out[1] = v ? (in[3] | (~in[3] & in[2])) : 1'bz;
+  // assign v = in[3] | in[2] | in[1] | in[0];
+  // assign out[0] = v ? (in[3] | (~in[2] & in[1])) : 1'bz;
+  // assign out[1] = v ? (in[3] | (~in[3] & in[2])) : 1'bz;
+always @(in) begin
+  if (in!=4'b0000) begin
+    v=1'b1;
+    out[0] = in[3] | (~in[2] & in[1]);
+    out[1] = in[3] | (~in[3] & in[2]);
   end
+  else begin
+    v=1'b0;
+    out=2'bzz;
+  end
+end
 
 endmodule
